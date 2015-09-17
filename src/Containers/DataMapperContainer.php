85f7319947e708d3ceebf800bb2f889ee36606bf
Sam Nicholson <snicholson@workanyware.co.uk>
2015-09-17 22:22:24 +0100

    Tidied up Data Mapper Container

--- src/Containers/DataMapperContainer.php
+++ src/Containers/DataMapperContainer.php
@@ -1,40 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: samdev
- * Date: 16/11/14
- * Time: 22:08
- */
 
 namespace SNicholson\IPFO\Containers;
-
 
 use SNicholson\IPFO\DataMappers\WIPODataMapper;
 use SNicholson\IPFO\DataMappers\EPODataMapper;
 use SNicholson\IPFO\DataMappers\USPTODataMapper;
 use SNicholson\IPFO\DataMappers\USPTOTrademarkDataMapper;
 
-class DataMapperContainer {
-
-    public function __construct(){
-
-    }
-
+class DataMapperContainer
+{
     /** @return EPODataMapper */
-    public function newEPODataMapper(){
+    public function newEPODataMapper()
+    {
         return new EPODataMapper();
     }
+
     /** @return USPTODataMapper */
-    public function newUSPTODataMapper(){
+    public function newUSPTODataMapper()
+    {
         return new USPTODataMapper();
     }
+
     /** @return USPTOTrademarkDataMapper */
-    public function newUSPTOTrademarkDataMapper(){
+    public function newUSPTOTrademarkDataMapper()
+    {
         return new USPTOTrademarkDataMapper();
     }
+
     /** @return USPTOTrademarkDataMapper */
-    public function newWIPODataMapper(){
+    public function newWIPODataMapper()
+    {
         return new WIPODataMapper();
     }
-
-} 
+}
